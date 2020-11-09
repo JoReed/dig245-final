3e879228ab333f336012d94ccb61e043417f1d4d
JoReed <59931206+JoReed@users.noreply.github.com>
2020-11-09 18:17:54 -0500

    update

---
+++ website/navigation/nav.js
@@ -0,0 +1,9 @@
+function nav() {
+  var nav = document.createElement("p")
+  var node = document.createTextNode("This is new.");
+  para.appendChild(node);
+
+  var element = document.getElementById("div1");
+  element.appendChild(para);
+
+}
