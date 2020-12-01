7befff62d58116f00e3050250d9f50f43f7a0a1d
JoReed <59931206+JoReed@users.noreply.github.com>
2020-12-01 18:52:43 -0500

    updated js

--- main.js
+++ main.js
@@ -7,5 +7,5 @@ function openBadWebsite() {
   var badWebsite = window.open("http://www.gatesnfences.com/", "_blank");
 }
 function openGoodWebsite() {
-  var goodWebsite = window.open("https://getbootstrap.com/docs/4.0/components/buttons/", "_blank");
+  var goodWebsite = window.open("https://aluminumfencesdirect.net", "_blank");
 }
