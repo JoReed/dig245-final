c2493f1974acb4bad67af6b6f22f8eb313d49934
JoReed <59931206+JoReed@users.noreply.github.com>
2020-12-01 18:41:24 -0500

    Updated final

---
+++ main.js
@@ -0,0 +1,11 @@
+function showSurvey() {
+    var T = document.getElementById("Survey");
+    T.style.display = "block";  // <-- Set it to block
+}
+
+function openBadWebsite() {
+  var badWebsite = window.open("http://www.gatesnfences.com/", "_blank");
+}
+function openGoodWebsite() {
+  var goodWebsite = window.open("https://getbootstrap.com/docs/4.0/components/buttons/", "_blank");
+}
