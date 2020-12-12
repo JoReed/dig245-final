ddfe952611dfa78dc8c55a038f49a65df429879b
JoReed <59931206+JoReed@users.noreply.github.com>
2020-12-11 19:18:22 -0500

    update 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,15 +21,15 @@ $(document).on('click','.submit',function(){
     var scoreOne = 0;
     var scoreTwo = 0;
 
-    for (int i = 1; i < 9; i++) {
-      var questionNum = "#Q" + str(i);
-      if (1 < 5){
-        scoreOne += $( questionNum ).val();
-      }
-      else {
-        scoreTwo += $( questionNum ).val();
-      }
-    }
-    console.log("Hello");
+    // for (int i = 1; i < 9; i++) {
+    //   var questionNum = "#Q" + str(i);
+    //   if (1 < 5){
+    //     scoreOne += $( questionNum ).val();
+    //   }
+    //   else {
+    //     scoreTwo += $( questionNum ).val();
+    //   }
+    // }
+    console.log($( "Q1" ).val());
     console.log(scoreOne, scoreTwo);
 });
